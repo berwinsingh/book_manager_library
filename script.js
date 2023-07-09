5b3e8551755cfb8d10ee2c5a46bf557123879e4c
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2023-07-09 23:50:17 +0530

    Completed the card design and added basic JS functionality

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const loginBtn = document.querySelector("#login-btn");
 const addBook = document.getElementById("add-books");
 
+const template = document.querySelector("template");
+const cloneCard = template.content.cloneNode(true);
+const bookName = cloneCard.getElementById("book-name");
+const coverImage = cloneCard.getElementById("cover-image");
+const pageNum = cloneCard.getElementById("page-num");
+const authorName = cloneCard.getElementById("author");
+const description = cloneCard.getElementById("description");
+
 //Add book names manually
 const ikigai = new Book("Ikigai","Hector Garcia", 192, false);
-const flashPoint = new Book("FlashPoint", "Geoff Johns", 176, true)
+const flashPoint = new Book("FlashPoint", "Geoff Johns", 176, true, "./images/Flashpoint.avif")
 
 let myLibrary = [ikigai, flashPoint]; //Books library
 
-
-function Book(name, author, pages, read = false){
+function Book(name, author, pages, read = false, cover){
     this.name =  name,
     this.author = author,
     this.pages =pages,
-    this.read = read
+    this.read = read,
+    this.cover = cover
+    this.description = function (){
+        return `${this.name} is written by ${this.author}, contains ${this.pages} no. of pages.`
+    }
 }
 
 function addToLibrary (){
